d06938e9b77732c29f6e9838fc163aa49a99e9e9
root <urangyorkwisd>
2022-09-12 08:31:18 -0700

    fix typo

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -12,13 +12,17 @@ int main(void)
 	int i;
 
 	for (i = 0; i < 10; i++)
+	{
 		putchar(a);
 		a++;
-	a = '1'
+	}
+	a = '1';
 
 	for (i = 0; i < 6; i++)
+	}
 		putchar('0' + a);
 		a++;
-
+	}
 	putchar('\n');
 	return (0);
+}
