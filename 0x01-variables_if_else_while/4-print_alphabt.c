c73a44a066a02d186a02e25bdd2d334b85dd8b85
root <urangyorkwisd>
2022-09-12 05:35:35 -0700

    putchar in c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+*
+* main - Print alphabet exempting q and e
+*
+* Return: always 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for(a='a', a <='z', a++)
+	{
+		if (a == 'q' || a == 'e')
+		{
+			continue
+		}
+		putchar(a);
+	}
+	putchar("\n")
+	return (0);
+}
