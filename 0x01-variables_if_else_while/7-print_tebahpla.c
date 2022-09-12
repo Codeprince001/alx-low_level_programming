35c03dde4d33cc3615654b3e04bf78cb567e984e
root <urangyorkwisd>
2022-09-12 07:59:30 -0700

    using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - entry point
+*
+* Return: 0
+*/
+
+int main(void)
+{
+	char a;
+
+	for (alpha = 'z'; alpha  >= 'a'; alpha--)
+		putchar(alpha)
+	putchar('\n');
+	return (0);
+}
