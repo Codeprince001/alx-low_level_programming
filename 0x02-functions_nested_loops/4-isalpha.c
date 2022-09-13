15d2cdbbabf4e5cb722be20958a41d4592d95d4f
root <urangyorkwisd>
2022-09-13 16:14:30 -0700

    initial commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* _isalpha - main function call
+* @c: parameter to compare
+* Return: 1 if True, 0 if False
+*/
+
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
+	{
+		return (1);
+	}
+	return (0):
+}
