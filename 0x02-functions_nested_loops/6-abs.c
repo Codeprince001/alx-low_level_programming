5d84e03b2ea718faa530c05e984e7d37590159f3
root <urangyorkwisd>
2022-09-13 16:37:22 -0700

    conditions

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+* _abs - main function call
+* @n: function parameter
+* Return: n or n * -1
+*/
+
+int _abs(int n)
+{
+	if (n > 0)
+		return (n);
+	else
+		return (n * -1);
+}
