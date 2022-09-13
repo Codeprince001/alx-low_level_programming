c23845b70233c3e73b22184be0cc9872659d3fd6
root <urangyorkwisd>
2022-09-13 15:11:07 -0700

    used nested while loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main"
+
+/**
+* print_alphabet_x10 - prints alphabets on ten different lines
+*
+* Return: Always 0
+*
+*/
+
+void print_alphabet_x10(void)
+{
+	char a = 'a';
+	char z = 'z';
+	int i = 0;
+
+	while (i < 10)
+	{
+		while (a < z)
+		{
+			_putchar(a);
+			a++;
+		}
+		_putchar('\n');
+	}
+}
