e90ac424b47dc0a6bd41b7330ab9c06d75842728
root <urangyorkwisd>
2022-09-13 18:53:14 -0700

    final commit

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -8,44 +8,32 @@
 
 void print_times_table(int n)
 {
-	int i, j;
-	
-	if (n <= 15 && n >= 9)
+	int prod, mult, num;
+
+	if (n <= 15 && n >= 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (num = 0; num <= n; ++num)
 		{
-			for (j = 0; j <= n; j++)
+			_putchar(48);
+			for (mult = 1; mult <= n; ++mult)
 			{
-				int product = j * i;
+				_putchar(',');
+				_putchar(' ');
 
-				if (j == 0)
-				{
-					_putchar('0');
-				} else if (product <= 9)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + product);
-				} else if (product > 99)
-				{
+				prod = num * mult;
 
-					_putchar(',');
+				if (prod <= 9)
 					_putchar(' ');
-					_putchar('0' + (product / 100));
-					_putchar('0' + ((product / 10) % 10));
-					_putchar('0' + (product % 10));
-				}
-				else
-				{
+				if (prod <= 99)
+					_putchar(' ');
 
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar('0' + (product / 10));
-					_putchar('0' + (product % 10));
-				}
+				if (prod >= 100)
+				{
+					_putchar((prod / 100) + 48);
+					_putchar((prod / 10) % 10 + 48);
+				} else if (prod <= 99 && prod >= 10)
+					_putchar((prod / 10) + 48);
+				_putchar((prod % 10) + 48);
 			}
 			_putchar('\n');
 		}
